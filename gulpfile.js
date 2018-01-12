266e2c99871f3dd173ad7a5274ad9247566c87dc
Palash Mondal <palashm@live.in>
2018-01-12 13:49:59 +0530

    Globbing patterns

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('hello', () => {
 
 gulp.task('sass', () => {
 	return gulp
-		.src('app/scss/styles.scss')
+		.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
 		.pipe(gulp.dest('app/css'));
 });
