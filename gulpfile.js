389fecca90b66d4c46543d119423497a2e7b336e
Palash Mondal <palashm@live.in>
2018-01-12 13:56:12 +0530

    Watching Sass files for changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,16 @@ gulp.task('hello', () => {
 	console.log('Hello World');
 });
 
+// Compile Sass to CSS with the help of a plugin called gulp-sass
 gulp.task('sass', () => {
 	return gulp
 		.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
 		.pipe(gulp.dest('app/css'));
+});
+
+// Watching Sass files for changes
+gulp.task('watch', () => {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	// Other watchers
 });
