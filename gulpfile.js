080ef76a56d62233a4a362521a60adf91e97d17c
Palash Mondal <palashm@live.in>
2018-01-12 16:04:03 +0530

    Live reloading with Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
+// Our first task
 gulp.task('hello', () => {
 	console.log('Hello World');
+});
+
+// Create a browserSync task to enable Gulp to spin up a server using Browser Sync
+gulp.task('browserSync', () => {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		}
+	});
 });
 
 // Compile Sass to CSS with the help of a plugin called gulp-sass
@@ -10,11 +21,20 @@ gulp.task('sass', () => {
 	return gulp
 		.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
-		.pipe(gulp.dest('app/css'));
+		.pipe(gulp.dest('app/css'))
+		.pipe(
+			browserSync.reload({
+				stream: true
+			})
+		);
 });
 
 // Watching Sass files for changes
-gulp.task('watch', () => {
+// browserSync and sass must be completed before watch is allowed to run.
+gulp.task('watch', ['browserSync', 'sass'], () => {
 	gulp.watch('app/scss/**/*.scss', ['sass']);
-	// Other watchers
+
+	// Reloads the browser whenever HTML or JS files change
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
 });
